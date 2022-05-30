6dc83f43160c3700df5e39dcd067847aae539a91
Muhammad Rizqullah Akbar <muhammadakbar.19051@mhs.its.ac.id>
2022-05-31 00:41:09 +0700

    Fix Create Category

--- src/resources/views/Layouts/main.blade.php
+++ src/resources/views/Layouts/main.blade.php
@@ -9,9 +9,9 @@
     <meta name="author" content="">
     <link href="img/logo/logo.png" rel="icon">
     <title>IMS | Dashboard</title>
-    <link href="dist/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="dist/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="dist/css/ruang-admin.min.css" rel="stylesheet">
+    <link href="{{ URL::asset('dist/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('dist/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('dist/css/ruang-admin.min.css') }}" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -39,12 +39,12 @@
             <i class="fas fa-angle-up"></i>
         </a>
 
-        <script src="dist/vendor/jquery/jquery.min.js"></script>
-        <script src="dist/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="dist/vendor/jquery-easing/jquery.easing.min.js"></script>
-        <script src="dist/js/ruang-admin.min.js"></script>
-        <script src="dist/vendor/chart.js/Chart.min.js"></script>
-        <script src="dist/js/demo/chart-area-demo.js"></script>
+        <script src="{{ URL::asset('dist/vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ URL::asset('dist/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ URL::asset('dist/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+        <script src="{{ URL::asset('dist/js/ruang-admin.min.js') }}"></script>
+        <script src="{{ URL::asset('dist/vendor/chart.js/Chart.min.js') }}"></script>
+        <script src="{{ URL::asset('dist/js/demo/chart-area-demo.js') }}"></script>
 </body>
 
 </html>
--- src/resources/views/Layouts/topbar.blade.php
+++ src/resources/views/Layouts/topbar.blade.php
@@ -30,7 +30,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="dist/img/boy.png" style="max-width: 60px">
+                <img class="img-profile rounded-circle" src="{{ URL::asset('dist/img/boy.png') }}" style="max-width: 60px">
                 <span class="ml-2 d-none d-lg-inline text-white small">{{ auth()->user()->name }}</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
