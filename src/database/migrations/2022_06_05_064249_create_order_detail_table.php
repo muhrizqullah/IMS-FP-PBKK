1dd4e2d3348d320e8666e0386819bfad861a2da0
Muhammad Rizqullah Akbar <muhammadakbar.19051@mhs.its.ac.id>
2022-06-05 16:10:11 +0700

    Fix Order Details Tables

--- src/database/migrations/2022_06_05_064249_create_order_detail_table.php
+++ src/database/migrations/2022_06_05_064249_create_order_detail_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('order_details', function (Blueprint $table) {
+            $table->id('id');
             $table->foreignId('order_id');
             $table->foreignId('product_id');
             $table->string('quantity');
