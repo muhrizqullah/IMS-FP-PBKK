99a8a178f0dfad577f95eb6207d55922fcd1deca
Muhammad Rizqullah Akbar <muhammadakbar.19051@mhs.its.ac.id>
2022-06-08 16:11:09 +0700

    Add App Logo

--- src/resources/views/Layouts/sidebar.blade.php
+++ src/resources/views/Layouts/sidebar.blade.php
@@ -1,10 +1,10 @@
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon">
             <img src="{{ URL::asset('dist/img/logo/logo2.png') }}">
         </div>
         <div class="sidebar-brand-text mx-3">IMS PBKK</div>
-    </a>
+    </div>
     <hr class="sidebar-divider my-0">
     <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
         <a class="nav-link" href="/">
